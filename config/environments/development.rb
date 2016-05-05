b7795e420a721702d43ddc7afe6263f6165c4b89
NgocSon2412 <sonnguyenk57@gmail.com>
2016-05-05 12:19:13 +0700

    host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,18 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = '153.120.171.194'
-  config.action_mailer.default_url_options = { host: host }
-   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'youremail@dommain.com',
-    :password       => 'youremailpassword',
-    :domain         => 'gmail.com',
-    :enable_starttls_auto => true
-  }
+   host = 'localhost:3000'
+    config.action_mailer.default_url_options = { host: host }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
